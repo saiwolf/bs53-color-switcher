3bf4b99ae9ac2fc1b36b80df00f9d064f03e26e9
Robert E. Cato <robertcato015@gmail.com>
2023-06-19 19:39:10 -0500

    Cleaning up project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import './style.css'
+import './style.scss'
+import * as bootstrap from 'bootstrap';
+
 import { colorModeInit } from './color-modes'
 
 colorModeInit()
 
+document.addEventListener('DOMContentLoaded', () => {
+     const popovers = document.querySelectorAll('[data-bs-toggle="popover"]');
+     if (popovers && popovers.length > 0) {
+          popovers.forEach(popover => {
+               new bootstrap.Popover(popover)
+          })
+     }
+})
